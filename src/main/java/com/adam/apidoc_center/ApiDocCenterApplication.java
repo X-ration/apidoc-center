1548fcb3a01fefb761f268f0788c351156b3117f
Adam <blacksheep9779@163.com>
2024-12-18 12:31:10 +0800

    init

---
+++ src/main/java/com/adam/apidoc_center/ApiDocCenterApplication.java
@@ -0,0 +1,13 @@
+package com.adam.apidoc_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDocCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adam/apidoc_center/ApiDocCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.apidoc_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
